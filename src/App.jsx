31786c155f716c2dddedd67d3a12b2267b6cbfaf
rmonges <robertoamonges@gmail.com>
2022-02-22 12:58:58 -0300

    creando app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import NavBar from './components/NavBar'
+import ItemListContainer from './container/ItemListContainer'
+
+const App = () => {
+  return (
+    <div>
+    <NavBar/>
+    <ItemListContainer/>
+      
+    </div>
+  )
+}
+
+export default App
