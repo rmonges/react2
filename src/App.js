1113db2fed8f8466f325c55b356cc8722fc61f58
rmonges <robertoamonges@gmail.com>
2022-02-22 12:00:54 -0300

    segundo commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         hola!
+         hola! como estan
         </p>
         <a
           className="App-link"
